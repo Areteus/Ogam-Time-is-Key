82942707aec9666a10709e0e7069a964ea91ba6c
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-06-05 02:19:10 -0400

    Wheel Collider positioning incorrect
    Wheel collider is facing diffrent direction from wheel need to fix on either blender or find some other way to fix it
    
    god Help us all........

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    //getting wheel collider for each wheel 
+    public WheelCollider FrontLeftWheel;
+    public WheelCollider FrontRightWheel;
+    public WheelCollider BackLeftWheel;
+    public WheelCollider BackRightWheel;
+
+    // accessesing Transfom of each Wheel 
+    public Transform FLW; //FrontLeftWheel
+    public Transform FRW; //FrontRightWheel
+    public Transform BLW; //BackLeftWheel
+    public Transform BRW; //BackRightWheel
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
