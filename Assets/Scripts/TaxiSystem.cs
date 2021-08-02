29a4589e9836c36cdde3381a8cfb2a01fd0c51c9
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-08-02 19:49:39 -0400

    passenger script WORKS!
    Passenger Script is now setting random waypoints and making new ones each time the player has picked up a passenger and placed
    
    **NOTES TO FIX**
    --Randomization:
    passenger script is showing the picked up location as a drop location.

---
+++ Assets/Scripts/TaxiSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaxiSystem : MonoBehaviour
+{
+    //find random waypoint 
+    //pick up passenger on waypoint 
+    //Find Destination and drop off passenger 
+    //Start over
+
+    [SerializeField] Transform[] Waypoints;
+
+    GameObject PickupWaypoint;
+    GameObject DestinationWaypoint; 
+
+    void Start()
+    {
+        PickupWaypoint = GetRandomWaypoints();
+        Debug.Log("go to " + PickupWaypoint); 
+        //DestinationWaypoint = GetRandomWaypoints();
+    }
+
+
+
+    public void PassengerPickUp(GameObject passingerPoint)
+    {
+        if (passingerPoint == PickupWaypoint)
+        {
+            Debug.Log("passenger picked up");
+            DestinationWaypoint = GetRandomWaypoints();
+            Debug.Log("go to " + DestinationWaypoint);
+        }
+
+        else if (passingerPoint == DestinationWaypoint)
+        {
+            //add time and score
+            Debug.Log("passenger droped"); 
+            PickupWaypoint = GetRandomWaypoints();
+            Debug.Log("go to " + PickupWaypoint);
+        }
+    }
+
+    private GameObject GetRandomWaypoints()
+    {
+        Transform selectedWaypoint;
+        int rand = Random.Range(0, Waypoints.Length);
+        selectedWaypoint = Waypoints[rand];
+        return selectedWaypoint.gameObject; 
+    }
+
+}
